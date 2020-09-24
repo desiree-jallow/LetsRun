adb3e9f35120b4916b5195e465ee351811b0a99c
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2020-09-24 12:36:11 -0500

    Initial Commit

---
+++ LetsRun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsRun
+//
+//  Created by Desiree on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
